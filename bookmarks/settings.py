714d6b477c53a02420c13042c720f8b16fb49641
Brian Ngeno <kbrianngeno@gmail.com>
2020-12-25 15:14:00 +0300

    Generate random secret key

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#jg0o6k#k+bx2o(!ugisz!lk98=+g&^uio#(0*uow^(occ&+9@'
+# SECRET_KEY = '#jg0o6k#k+bx2o(!ugisz!lk98=+g&^uio#(0*uow^(occ&+9@'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
